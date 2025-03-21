a32dec606d1c206d092562a1538cfd6cc08c807c
Олексій Савін <96502466+Oleksii-Savin@users.noreply.github.com>
2025-03-21 22:31:24 +0200

    fixed task1
    fixed task3

--- task-1.js
+++ task-1.js
@@ -1,7 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
   let totalPrice = quantyty * pricePerDroid;
-  return;
-  `Your ordered ${quantity} droids worth ${totalPrice} credits`;
+  return `Your ordered ${quantity} droids worth ${totalPrice} credits`;
 }
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
